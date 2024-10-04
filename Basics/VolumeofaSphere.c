59ca73c4f33166591f3fcc33ea66f28bcbf3b4d4
Oindrila Majumder <oindrilamajumder3@gmail.com>
2024-10-04 23:45:35 +0530

    Update

--- Basics/VolumeofaSphere.c
+++ Basics/VolumeofaSphere.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
-#include<math.h>
-int main()
-{
-    float radius;
+int main(){
+    int r;
     printf("Enter the radius of the sphere: ");
-    scanf("%f",&radius);
-    float volume, pi;
-    pi = 3.14;
-    volume = (4*pi*radius*radius*radius)/3;
-    printf("The volume of the sphere is: %f",volume);
+    scanf("%d",&r);
+    float volume = 4 * 3.14 * r * r * r / 3;
+    printf("The volume of the sphere is : %f",volume);
     return 0;
 }
