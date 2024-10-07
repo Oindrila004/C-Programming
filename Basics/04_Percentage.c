9cfa222dfd5873a293188da79cb671c482ea8dd8
Oindrila Majumder <oindrilamajumder3@gmail.com>
2024-10-07 20:13:17 +0530

    Update

--- Basics/04_Percentage.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float sub1 = 92, sub2 = 97, sub3 = 85, sub4 = 92, sub5 = 86;
-    float percentage;
-    percentage = (sub1 + sub2 + sub3 + sub4 + sub5)/5;
-    printf("The overall percentage of your marks is: %f",percentage);
-    return 0;
-}
--- Basics/09_Character.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char ch = 'O';
-    printf("%c",ch);
-    return 0;
-}
-//ASCII values: A - Z = 65 - 90, a - z = 97 - 122
