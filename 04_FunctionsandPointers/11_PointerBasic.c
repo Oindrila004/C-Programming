20bb09960f6ee6fcd7ecfc540ab66aed7709da79
Oindrila Majumder <oindrilamajumder3@gmail.com>
2024-12-28 21:39:23 +0530

    Update

--- 04_FunctionsandPointers/11_PointerBasic.c
+++ 04_FunctionsandPointers/11_PointerBasic.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int x;
-    int* ptr = &x;
+    int* ptr = &x;//* is value at address operator
     printf("\n%p\n\n",ptr);//using pointer to print the address of x variable
     return 0;
 }
